b8490f924eda6a5037e8709cac36474116e71952
fashionkilla5000 <44493009+fashionkilla5000@users.noreply.github.com>
2019-10-24 12:02:26 +0200

    Ćwiczenie 13

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uczę_się
+{
+	public class Car
+	{
+		static int iloscKol;
+		private double pojemnoscSilnika;
+		private string marka;
+		public Car(double pojemnoscSilnika, string marka)
+		{
+			
+			this.pojemnoscSilnika = pojemnoscSilnika;
+			this.marka = marka;
+		}
+		static public Car Create(double pojemnoscSilnika, string marka)
+		{
+			return new Car(pojemnoscSilnika, marka);
+		}
+		~Car()
+		{
+			Console.WriteLine("Zwalniam pamięć");
+			Console.ReadKey();
+		}
+		static Car()
+		{
+			iloscKol = 4;
+		}
+		public void wypisz()
+		{
+			Console.WriteLine("pojemnosc: " + this.pojemnoscSilnika + "marka: " + this.marka + "iloscKol: "+iloscKol);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uczę_się
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Car pierwszy = new Car(1.9, "Focus");
+			Car drugi = new Car(2.0, "Mazda");
+
+			pierwszy.wypisz();
+		}
+	}
+}
